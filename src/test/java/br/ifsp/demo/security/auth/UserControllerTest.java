a93070a2d348a771d7dfe1b044b4ea989700562f
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-06-11 20:16:17 -0300

    style: improve code formatting and indentation

--- src/test/java/br/ifsp/demo/security/auth/UserControllerTest.java
+++ src/test/java/br/ifsp/demo/security/auth/UserControllerTest.java
@@ -19,9 +19,16 @@ class UserControllerTest extends BaseApiIntegrationTest {
     @DisplayName("Should register user")
     void shouldRegisterUser(){
         final User user = EntityBuilder.createRandomUser("123");
-        given().contentType("application/json").port(RestAssured.port).body(user)
-                .when().post("api/v1/register")
-                .then().log().ifValidationFails(LogDetail.BODY).statusCode(201).body("id", notNullValue());
+
+        given()
+            .contentType("application/json").port(port).body(user)
+        .when()
+                .post("/api/v1/register")
+        .then()
+                .log()
+                .ifValidationFails(LogDetail.BODY)
+                .statusCode(201)
+                .body("id", notNullValue());
     }
 
     @Test
@@ -32,8 +39,16 @@ class UserControllerTest extends BaseApiIntegrationTest {
         final String plainTextPassword = "123password";
         final User user = registerUser(plainTextPassword);
         AuthRequest authRequest = new AuthRequest(user.getEmail(), plainTextPassword);
-        given().contentType("application/json").port(port).body(authRequest).
-                when().post("/api/v1/authenticate").
-                then().log().ifValidationFails(LogDetail.BODY).statusCode(200).body("token", notNullValue());
+        given()
+                .contentType("application/json")
+                .port(port)
+                .body(authRequest).
+        when().
+                post("/api/v1/authenticate").
+        then()
+                .log()
+                .ifValidationFails(LogDetail.BODY)
+                .statusCode(200)
+                .body("token", notNullValue());
     }
 }
