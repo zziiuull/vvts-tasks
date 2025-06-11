c2a7325b017f713a968bfc88474d7492d1988abc
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-11 20:40:23 -0300

    refactor: Update IncludeTags

--- src/test/java/br/ifsp/demo/suits/AllPersistenceTests.java
+++ src/test/java/br/ifsp/demo/suits/AllPersistenceTests.java
@@ -8,6 +8,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("br.ifsp.demo.tasks")
 @SuiteDisplayName("All Persistence Tests")
-@IncludeTags({"PersistenceTests"})
+@IncludeTags({"PersistenceTest"})
 public class AllPersistenceTests {
 }
