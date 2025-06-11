3a1fce18640591da4ad1f0d2bb9c5eb6370ed03c
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-06-11 20:15:08 -0300

    feat: add method to create random CreateTaskDTO instances

--- src/test/java/br/ifsp/demo/controller/EntityBuilder.java
+++ src/test/java/br/ifsp/demo/controller/EntityBuilder.java
@@ -2,9 +2,14 @@ package br.ifsp.demo.controller;
 
 import br.ifsp.demo.security.user.Role;
 import br.ifsp.demo.security.user.User;
+import br.ifsp.demo.tasks.dtos.CreateTaskDTO;
 import com.github.javafaker.Faker;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class EntityBuilder {
     private static final Faker faker = new Faker();
@@ -19,5 +24,19 @@ public class EntityBuilder {
                 .password(password)
                 .role(Role.USER)
                 .build();
+    }
+
+    public static CreateTaskDTO createRandomCreateTaskDTO() {
+        Date futureDate = faker.date().future(10, TimeUnit.DAYS);
+        LocalDateTime deadline = futureDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        long estimatedTime = faker.number().numberBetween(10, 60);
+
+        return new CreateTaskDTO(
+                faker.name().title(),
+                faker.lorem().sentence(10),
+                deadline,
+                estimatedTime,
+                faker.hipster().word()
+        );
     }
 }
