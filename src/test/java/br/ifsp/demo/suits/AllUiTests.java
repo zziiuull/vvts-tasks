e38dcf45976d16e4ade8f7803f249452bfd3844d
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-11 20:43:31 -0300

    feat: add suite for UI tests

---
+++ src/test/java/br/ifsp/demo/suits/AllUiTests.java
@@ -0,0 +1,13 @@
+package br.ifsp.demo.suits;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages("br.ifsp.demo.tasks")
+@SuiteDisplayName("All UI Tests")
+@IncludeTags({"UiTest"})
+public class AllUiTests {
+}
