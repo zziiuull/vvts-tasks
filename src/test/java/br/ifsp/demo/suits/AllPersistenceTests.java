065719512fd7c88820eaeb0e2d6cd950d157a366
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-11 20:39:50 -0300

    feat: add suite for persistence tests

--- src/test/java/br/ifsp/demo/suits/AllPersistenceTests.java
+++ src/test/java/br/ifsp/demo/suits/AllPersistenceTests.java
@@ -1,2 +1,13 @@
-package br.ifsp.demo.suits;public class AllPersistenceTests {
+package br.ifsp.demo.suits;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages("br.ifsp.demo.tasks")
+@SuiteDisplayName("All Persistence Tests")
+@IncludeTags({"PersistenceTests"})
+public class AllPersistenceTests {
 }
