79830c80af4e10e49e7c2bbbacc4371f20a3bacd
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-12 19:22:10 -0300

    test: add persistence test to verify if return all tasks for given user

--- src/test/java/br/ifsp/demo/tasks/JpaTaskRepositoryTest.java
+++ src/test/java/br/ifsp/demo/tasks/JpaTaskRepositoryTest.java
@@ -1,13 +1,14 @@
 package br.ifsp.demo.tasks;
 
 import br.ifsp.demo.util.TaskBuilder;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -17,16 +18,31 @@ class JpaTaskRepositoryTest {
     @Autowired
     private JpaTaskRepository taskRepository;
 
+    private UUID userA;
+    private UUID userB;
+    private TaskEntity taskA;
+
+
     @BeforeEach
     void setup() {
         taskRepository.deleteAll();
 
-        UUID userA = UUID.randomUUID();
-        UUID userB = UUID.randomUUID();
+        userA = UUID.randomUUID();
+        userB = UUID.randomUUID();
 
-        TaskEntity taskA = TaskBuilder.defaultTask(userA);
+        taskA = TaskBuilder.defaultTask(userA);
         taskRepository.save(taskA);
     }
 
+    @Test
+    @Tag("PersistenceTest")
+    @Tag("IntegrationTest")
+    @DisplayName("Should return all tasks for given user")
+    void shouldReturnAllTasksForGivenUser() {
+        List<TaskEntity> result = taskRepository.findAllByUserId(userA);
+
+        assertThat(result).hasSize(1);
+        assertThat(result.getFirst().getUserId()).isEqualTo(userA);
+    }
 
 }
