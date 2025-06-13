4c3f2b89fa6dd7d5060198f480c816082e4b3409
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-13 18:43:01 -0300

    feat: add waitForErrorMessage method

--- src/test/java/br/ifsp/demo/ui/pages/LoginPageObject.java
+++ src/test/java/br/ifsp/demo/ui/pages/LoginPageObject.java
@@ -2,6 +2,11 @@ package br.ifsp.demo.ui.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPageObject extends BasePageObject {
     private static final String PAGE_TITLE = "Login";
@@ -34,5 +39,11 @@ public class LoginPageObject extends BasePageObject {
 
     public void clickRegister() {
         driver.findElement(registerButton).click();
+    }
+
+    public String waitForErrorMessage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement error = wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage));
+        return error.getText();
     }
 }
