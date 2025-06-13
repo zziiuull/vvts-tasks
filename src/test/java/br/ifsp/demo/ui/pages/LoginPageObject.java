28c298ed4f4f259ebd62e9fe4e8f573c3c241d2d
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-13 18:35:49 -0300

    feat: create LoginPageObject

---
+++ src/test/java/br/ifsp/demo/ui/pages/LoginPageObject.java
@@ -0,0 +1,20 @@
+package br.ifsp.demo.ui.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPageObject extends BasePageObject {
+    private static final String PAGE_TITLE = "Login";
+
+    public LoginPageObject(WebDriver driver) {
+        super(driver);
+        if(!PAGE_TITLE.equals(pageTitle()))
+            throw new IllegalStateException("Wrong page url:" + driver.getCurrentUrl());
+    }
+
+    private final By usernameField = By.id("username");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.cssSelector("button[type='submit']");
+    private final By registerButton = By.id("toRegisterBtn");
+    private final By errorMessage = By.id("errorMessage");
+}
