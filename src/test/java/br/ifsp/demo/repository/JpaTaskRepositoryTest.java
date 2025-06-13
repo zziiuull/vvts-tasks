f603057661bc879f8f42afebe5fbafbbab3d7865
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-13 00:37:45 -0300

    refactor: add ActiveProfiles to use application-test.properties and h2 database

--- src/test/java/br/ifsp/demo/repository/JpaTaskRepositoryTest.java
+++ src/test/java/br/ifsp/demo/repository/JpaTaskRepositoryTest.java
@@ -6,6 +6,7 @@ import br.ifsp.demo.util.TaskBuilder;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JpaTaskRepositoryTest {
 
