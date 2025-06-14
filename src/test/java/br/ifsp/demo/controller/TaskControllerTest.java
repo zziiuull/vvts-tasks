42589dabbbf9104b10660e1dedd19a65fc27aebb
Shoji <hshojikato@gmail.com>
2025-06-14 10:36:33 -0300

    test: add integration test for checking clock out of non existent task

--- src/test/java/br/ifsp/demo/controller/TaskControllerTest.java
+++ src/test/java/br/ifsp/demo/controller/TaskControllerTest.java
@@ -1097,6 +1097,22 @@ class TaskControllerTest extends BaseApiIntegrationTest {
             }
         }
 
-        
+        @Nested
+        @DisplayName("404 not found")
+        class NotFound {
+            @Test
+            @Tag("ApiTest")
+            @Tag("IntegrationTest")
+            @DisplayName("should return not found status code when checking a non existent task")
+            void shouldReturnNotFoundStatusCodeWhenCheckingANonExistentTask() {
+                given()
+                        .port(RestAssured.port)
+                        .header("Authorization", authorizationHeader)
+                        .when()
+                        .get("/api/v1/task/clock-out-forgotten/" + UUID.randomUUID())
+                        .then()
+                        .statusCode(HttpStatus.NOT_FOUND.value());
+            }
+        }
     }
 }
