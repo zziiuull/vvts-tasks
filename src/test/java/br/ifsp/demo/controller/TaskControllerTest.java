28590ea55b0a765d21375953661dfdf247aac5e7
Shoji <hshojikato@gmail.com>
2025-06-14 10:44:53 -0300

    test: add integration test for checking non existent task

--- src/test/java/br/ifsp/demo/controller/TaskControllerTest.java
+++ src/test/java/br/ifsp/demo/controller/TaskControllerTest.java
@@ -1148,6 +1148,22 @@ class TaskControllerTest extends BaseApiIntegrationTest {
             }
         }
 
-
+        @Nested
+        @DisplayName("404 not found")
+        class NotFound {
+            @Test
+            @Tag("ApiTest")
+            @Tag("IntegrationTest")
+            @DisplayName("should return not found status code when checking a non existent task")
+            void shouldReturnNotFoundStatusCodeWhenCheckingANonExistentTask() {
+                given()
+                        .port(RestAssured.port)
+                        .header("Authorization", authorizationHeader)
+                        .when()
+                        .get("/api/v1/task/clock-out-forgotten-completed-task/" + UUID.randomUUID())
+                        .then()
+                        .statusCode(HttpStatus.NOT_FOUND.value());
+            }
+        }
     }
 }
