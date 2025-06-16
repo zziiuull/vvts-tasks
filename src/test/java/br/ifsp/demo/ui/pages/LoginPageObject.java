a842e436b055bb8fbdb1e51c6eb4da3dae4896ea
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-16 00:45:25 -0300

    feat: add method in LoginPageObject to wait for password error

--- src/test/java/br/ifsp/demo/ui/pages/LoginPageObject.java
+++ src/test/java/br/ifsp/demo/ui/pages/LoginPageObject.java
@@ -55,6 +55,12 @@ public class LoginPageObject extends BasePageObject {
                 .visibilityOfElementLocated(By.id("username-error"))).getText();
     }
 
+    public String waitForPasswordError() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        return wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.id("password-error"))).getText();
+    }
+
     public String waitForErrorMessage() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         WebElement error = wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage));
