84ce49742158087c1ebdbf2a9320adf2fff19665
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-06-16 17:39:41 -0300

    test: add validation for past deadline in edit task functionality

--- src/test/java/br/ifsp/demo/ui/EditTaskTest.java
+++ src/test/java/br/ifsp/demo/ui/EditTaskTest.java
@@ -5,10 +5,12 @@ import br.ifsp.demo.ui.utils.Auth;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -346,5 +348,83 @@ public class EditTaskTest extends BaseSeleniumTest {
 
         assertThat(editTaskPageObject.getErrorMessage()).isEqualTo("All fields are required.");
         assertThat(driver.getTitle()).isEqualTo(EditTaskPageObject.PAGE_TITLE);
+    }
+
+    @Test
+    @Tag("UiTest")
+    @DisplayName("Should not edit a task and show alert when deadline is in past")
+    void shouldNotEditATaskAndShowAlertWhenDeadlineIsInPast(){
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password();
+        var taskListPage = Auth.registerAndLogin(driver, email, password);
+
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofMillis(300))
+                .ignoring(NoSuchElementException.class)
+                .until(ExpectedConditions.elementToBeClickable(taskListPage.byCreateTask()));
+
+        var createTaskPage = taskListPage.navigateToCreateTaskPage();
+
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class)
+                .until(ExpectedConditions.elementToBeClickable(createTaskPage.byCreateButton()));
+
+        String title = faker.name().title();
+        createTaskPage.fillTaskTitle(title);
+
+        String description = faker.lorem().sentence();
+        createTaskPage.fillTaskDescription(description);
+
+        Date futureDate = faker.date().past(365, TimeUnit.DAYS);
+        LocalDateTime futureDateTime = futureDate.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("ddMMyyyy");
+        String date = futureDateTime.format(dateFormatter);
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm");
+        String time = futureDateTime.format(timeFormatter);
+        createTaskPage.fillTaskDeadline(date, time);
+
+        taskListPage = createTaskPage.submitTask();
+
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class)
+                .until(ExpectedConditions.elementToBeClickable(taskListPage.byCreateTask()));
+
+        var taskPage = taskListPage.navigateToTaskPage(title);
+
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class)
+                .until(ExpectedConditions.presenceOfElementLocated(taskPage.byTaskTitle()));
+
+        EditTaskPageObject editTaskPageObject = taskPage.editTask();
+
+        String newTaskTitle = faker.name().title();
+        String newDescription = faker.lorem().sentence(3);
+        Date newPastDate = faker.date().past(365, TimeUnit.DAYS);
+        LocalDateTime newPastDateTime = newPastDate.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+        String newDate = newPastDateTime.format(dateFormatter);
+        String newTime = newPastDateTime.format(timeFormatter);
+
+        editTaskPageObject.fillTaskTitleInput(newTaskTitle);
+        editTaskPageObject.fillTaskDescriptionInput(newDescription);
+        editTaskPageObject.fillTaskDeadlineInput(newDate, newTime);
+
+        editTaskPageObject.tryEditTask();
+
+        final Alert alert = new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.alertIsPresent());
+        final String alertText = alert.getText();
+
+        assertThat(alertText).isEqualTo("Error editing task. Please try again later.");
     }
 }
