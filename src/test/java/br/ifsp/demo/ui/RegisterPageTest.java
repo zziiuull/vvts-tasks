3b3ddf117e4369b77751b0b850247b742847c529
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-16 21:42:08 -0300

    test: add test to register user successfully

--- src/test/java/br/ifsp/demo/ui/RegisterPageTest.java
+++ src/test/java/br/ifsp/demo/ui/RegisterPageTest.java
@@ -2,7 +2,15 @@ package br.ifsp.demo.ui;
 
 import br.ifsp.demo.ui.pages.LoginPageObject;
 import br.ifsp.demo.ui.pages.RegisterPageObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterPageTest extends BaseSeleniumTest {
@@ -11,9 +19,29 @@ class RegisterPageTest extends BaseSeleniumTest {
 
     @Override
     protected void setInitialPage() {
-        driver.get("http://localhost:8081/index.html");
+        driver.get("http://localhost:8081/register.html");
         registerPage = new RegisterPageObject(driver);
     }
 
+    @Test
+    @Tag("UiTest")
+    @DisplayName("Should register user successfully")
+    void shouldRegisterUserSuccessfully() {
+        String name = faker.name().firstName();
+        String lastname = faker.name().lastName();
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password(6, 20);
 
+        registerPage.fillName(name);
+        registerPage.fillLastnameField(lastname);
+        registerPage.fillEmail(email);
+        registerPage.fillPassword(password);
+
+        LoginPageObject loginPage = registerPage.clickRegister();
+
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.titleIs(LoginPageObject.PAGE_TITLE));
+
+        assertThat(loginPage.pageTitle()).isEqualTo(LoginPageObject.PAGE_TITLE);
+    }
 }
