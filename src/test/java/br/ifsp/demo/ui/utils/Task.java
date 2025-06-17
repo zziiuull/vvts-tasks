9d76b9d27f31cf8ac35a8a149cb75c14cff4ac3a
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-06-17 16:14:17 -0300

    feat: add Task utility class for task creation functionality

---
+++ src/test/java/br/ifsp/demo/ui/utils/Task.java
@@ -0,0 +1,29 @@
+package br.ifsp.demo.ui.utils;
+
+import br.ifsp.demo.ui.CreateTaskTest;
+import br.ifsp.demo.ui.pages.CreateTaskPageObject;
+import br.ifsp.demo.ui.pages.TaskListPageObject;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
+
+public class Task {
+    public static TaskListPageObject createTask(WebDriver driver, TaskListPageObject taskListPageObject, String title, String description, String date, String time){
+        CreateTaskPageObject createTaskPageObject = taskListPageObject.navigateToCreateTaskPage();
+
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class)
+                .until(ExpectedConditions.titleIs(CreateTaskPageObject.PAGE_TITLE));
+
+        createTaskPageObject.fillTaskTitle(title);
+        createTaskPageObject.fillTaskDescription(description);
+        createTaskPageObject.fillTaskDeadline(date, time);
+
+        return createTaskPageObject.submitTask();
+    }
+}
