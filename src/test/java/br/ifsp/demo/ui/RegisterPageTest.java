fd50e3bd076aa510e6ac783b24aaad6848065bf2
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-17 18:20:43 -0300

    test: add parameterized test to show error when email is invalid

--- src/test/java/br/ifsp/demo/ui/RegisterPageTest.java
+++ src/test/java/br/ifsp/demo/ui/RegisterPageTest.java
@@ -5,13 +5,16 @@ import br.ifsp.demo.ui.pages.RegisterPageObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterPageTest extends BaseSeleniumTest {
 
@@ -43,5 +46,37 @@ class RegisterPageTest extends BaseSeleniumTest {
                 .until(ExpectedConditions.titleIs(LoginPageObject.PAGE_TITLE));
 
         assertThat(loginPage.pageTitle()).isEqualTo(LoginPageObject.PAGE_TITLE);
+    }
+
+    @ParameterizedTest(name = "[{index}] Invalid email: {0}")
+    @CsvSource({
+            "user@mail",
+            "user@.com",
+            "user@com.",
+            ".@mail.com",
+            "?@mail.com",
+            "user@@mail.com",
+            "<>@mail.com"
+    })
+    @Tag("UiTest")
+    @DisplayName("Should show error for invalid email format")
+    void shouldShowErrorForInvalidEmailFormat(String email) {
+        String name = faker.name().firstName();
+        String lastname = faker.name().lastName();
+        String password = faker.internet().password(6, 20);
+
+        registerPage.fillName(name);
+        registerPage.fillLastnameField(lastname);
+        registerPage.fillEmail(email);
+        registerPage.fillPassword(password);
+
+        registerPage.clickRegister();
+
+        WebElement emailError = new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("email-error")));
+
+        assertThat(emailError.getText())
+                .as("Esperava mensagem para e-mail inválido: %s", email)
+                .contains("Invalid email format.");
     }
 }
